c050c8800f98a858d8bd995b0cb5841f78b26d8e
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-02-01 22:22:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,13 +39,14 @@ while True:
                 input("Maximum of range within which random numbers will be generated? "))
             arr = [random.randint(min, max) for n in range(count)]
             print('The present array is:', arr)
+            
             print("\nSorting in progress..")
             time.sleep(5)
 
         if array_option == 3:
             quit()
 
-        # sorting the array with 4 sort algorithms, timing the sort and printing results
+        # sorting the array with 5 sort algorithms, timing the sort and printing results
         print("\nRESULTS \n")
 
         arr1 = arr
